0c9cedf95f1eba99ea1e79567db1bba35f3c40ba
Memo Akten <memo@memo.tv>
2019-06-23 23:14:02 +0300

    example update, display device specs, gui for device parameters

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,14 +1,57 @@
 #include "ofMain.h"
 #include "ofxSpinnaker.h"
 #include "ofxMachineVision.h"
-//#include "ofxGui.h"
+#include "ofxGui.h"
+
+
+// I don't like this, but couldnt' think of another way
+class DeviceParams {
+public:
+
+	// contains device parameters as ofParameters
+	ofParameterGroup pGroup;
+
+	// given the name of a device parameter, returns the corresponding ofxMachineVision::AbstractParameter
+	map<string, shared_ptr<ofxMachineVision::AbstractParameter>> mvpMap;
+
+	// add a device parameter to both the ofParameterGroup and the ofxMachineVision::AbstractParameter map
+	void add(shared_ptr<ofxMachineVision::AbstractParameter> deviceMVParam) {
+		ofAbstractParameter& deviceParam = deviceMVParam->getParameter();
+		ofLogVerbose() << "Device parameter : " << deviceParam.getName() << " " << deviceParam.type() << " " << deviceParam.valueType();
+		auto key = deviceParam.getName();
+		if (mvpMap.count(key) > 0) {
+			ofLogError() << deviceParam.getName() << " already exists.";
+		}
+		else {
+			pGroup.add(deviceParam); // add to ofParameterGroup for display in GUI
+			mvpMap[key] = deviceMVParam; // save to map for inverse retrieval
+		}
+	}
+
+	// setup a listener to sync changes in the ofParameterGroup to the device
+	// TODO, otherway sync?
+	void startListening() {
+		ofAddListener(pGroup.parameterChangedE(), this, &DeviceParams::onParameterChanged);
+	}
+
+private:
+	//--------------------------------------------------------------
+	void onParameterChanged(ofAbstractParameter& e) {
+		mvpMap[e.getName()]->syncToDevice();
+	}
+};
+
 
 class ofApp : public ofBaseApp {
 
 	ofxMachineVision::SimpleGrabber<ofxMachineVision::Device::Spinnaker> grabber;
-	//string specsStr;
-	ofParameterGroup parameters;
-	//ofxPanel gui;
+	
+	// Add device parameters to a gui
+	ofxPanel gui;
+	ofXml settingsXml;
+
+	DeviceParams deviceParams;
+
 
 	string getInfo(ofxMachineVision::SimpleGrabber<ofxMachineVision::Device::Spinnaker>& grabber) {
 		auto specs = grabber.getDeviceSpecification();
@@ -25,6 +68,8 @@ class ofApp : public ofBaseApp {
 
 	//--------------------------------------------------------------
 	void setup() {
+		ofSetLogLevel(OF_LOG_VERBOSE);
+
 		cout << "Listing devices..." << endl;
 		auto deviceDescriptions = grabber.getDevice()->listDevices();
 		for (auto&& deviceDescription : deviceDescriptions) {
@@ -39,15 +84,18 @@ class ofApp : public ofBaseApp {
 		ofSetWindowTitle(info);
 
 		cout << "Parameters:" << endl;
-		auto params = grabber.getDeviceParameters();
-		for (auto&& param : params) {
-			auto& p = param->getParameter();
-			cout << p.getName() << " " << p.type() << " " << p.valueType() << endl;
-		}
+		auto deviceMVParamsList = grabber.getDeviceParameters();
+		for (auto&& deviceMVParam : deviceMVParamsList) deviceParams.add(deviceMVParam);
+		deviceParams.startListening(); // important! sets listener to sync changes in gui to the device
+
+		gui.setup(deviceParams.pGroup);
+		gui.loadFromFile("settings.xml");
 
 		cout << "Starting Capture..." << endl;
 		grabber.startCapture();
 	}
+
+
 
 	//--------------------------------------------------------------
 	void update() {
@@ -59,11 +107,19 @@ class ofApp : public ofBaseApp {
 		grabber.draw(0, 0);
 		ofDrawBitmapStringHighlight(ofToString(grabber.getFps()), 10, 10);
 		ofSetWindowTitle(getInfo(grabber));
+		gui.draw();
 	}
 
 	//--------------------------------------------------------------
 	void keyPressed(int key) {
-
+		if (key == 's') {
+			ofSerialize(settingsXml, deviceParams.pGroup);
+			settingsXml.save("settings.xml");
+		}
+		if (key == 'l') {
+			settingsXml.load("settings.xml");
+			ofDeserialize(settingsXml, deviceParams.pGroup);
+		}
 	}
 };
 
